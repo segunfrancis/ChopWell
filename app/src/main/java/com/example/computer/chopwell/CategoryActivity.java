b9708a41dc1743a409c432fd808de1b092abad81
Segun Francis <francis4dblues@gmail.com>
2019-05-21 16:38:35 +0100

    Add StartActivity

--- app/src/main/java/com/example/computer/chopwell/CategoryActivity.java
+++ app/src/main/java/com/example/computer/chopwell/CategoryActivity.java
@@ -2,16 +2,45 @@ package com.example.computer.chopwell;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.example.computer.chopwell.adapter.MealAdapter;
+import com.example.computer.chopwell.model.MealModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryActivity extends AppCompatActivity {
+
+    private List<MealModel> modelList;
+    private MealAdapter searchAdapter;
+    private RecyclerView searchResults;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        searchResults = findViewById(R.id.search_recycler_view);
+        searchResults.setHasFixedSize(true);
+        searchResults.setLayoutManager(new LinearLayoutManager(CategoryActivity.this));
+
+        modelList = new ArrayList<>();
     }
 
     public void openBeveragesActivity(View view) {
@@ -44,5 +73,55 @@ public class CategoryActivity extends AppCompatActivity {
 
     public void openSoupsActivity(View view) {
         startActivity(new Intent(CategoryActivity.this, SoupsActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        // Implement SearchView
+        MenuItem item = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                Query query = FirebaseDatabase.getInstance().getReference("meals")
+                        .orderByChild("mealName")
+                        .startAt(s)
+                        .endAt(s + "\uf8ff");
+                modelList.clear();
+
+                query.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                            MealModel model = snapshot.getValue(MealModel.class);
+                            modelList.add(model);
+                        }
+                        searchAdapter = new MealAdapter(CategoryActivity.this, modelList);
+                        searchResults.setAdapter(searchAdapter);
+                        searchAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        modelList.clear();
+                    }
+                });
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
     }
 }
