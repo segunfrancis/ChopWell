277ad20ce108adf7397263e1f888963339994990
segunfrancis <francis4dblues@gmail.com>
2020-02-08 12:47:42 +0100

    ChopWell : onBoarding screen part 2

--- app/src/main/java/com/example/computer/chopwell/OnBoardingActivity.java
+++ app/src/main/java/com/example/computer/chopwell/OnBoardingActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.computer.chopwell.adapter.OnBoardingPagerAdapter;
 import com.example.computer.chopwell.model.OnBoardingModel;
@@ -18,6 +19,9 @@ import java.util.List;
 public class OnBoardingActivity extends AppCompatActivity {
 
     private final static String TAG = OnBoardingActivity.class.getSimpleName();
+    private ImageView mIndicator1;
+    private ImageView mIndicator2;
+    private ImageView mIndicator3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +31,17 @@ public class OnBoardingActivity extends AppCompatActivity {
         ViewPager2 onBoardingViewPager = findViewById(R.id.onBoarding_viewPager);
         AppCompatButton prevButton = findViewById(R.id.button_prev);
         AppCompatButton nextButton = findViewById(R.id.button_next);
+        mIndicator1 = findViewById(R.id.indicator1);
+        mIndicator2 = findViewById(R.id.indicator2);
+        mIndicator3 = findViewById(R.id.indicator3);
 
         // Populate OnBoarding screen data
         List<OnBoardingModel> modelList = new ArrayList<>();
-        OnBoardingModel onBoardingModel1 = new OnBoardingModel(R.drawable.undraw_healthy, "Have access to various Nigerian meals");
+        OnBoardingModel onBoardingModel1 = new OnBoardingModel(R.drawable.undraw_healthy, getResources().getString(R.string.first_onboarding_text));
         modelList.add(onBoardingModel1);
-        OnBoardingModel onBoardingModel2 = new OnBoardingModel(R.drawable.undraw_diet, "See how they are prepared");
+        OnBoardingModel onBoardingModel2 = new OnBoardingModel(R.drawable.undraw_diet, getResources().getString(R.string.second_onboarding_text));
         modelList.add(onBoardingModel2);
-        OnBoardingModel onBoardingModel3 = new OnBoardingModel(R.drawable.undraw_eating, "Prepare Nigerian meals and enjoy with friends and family");
+        OnBoardingModel onBoardingModel3 = new OnBoardingModel(R.drawable.undraw_eating, getResources().getString(R.string.third_onboarding_text));
         modelList.add(onBoardingModel3);
 
         OnBoardingPagerAdapter adapter = new OnBoardingPagerAdapter(modelList);
@@ -64,16 +71,19 @@ public class OnBoardingActivity extends AppCompatActivity {
                 switch (position) {
                     case 0:
                         hideButton(prevButton);
-                        nextButton.setText("NEXT");
+                        nextButton.setText(R.string.next_onboarding_text);
+                        indicatorSelector(true, false, false);
                         break;
                     case 1:
                         showButton(prevButton);
                         showButton(nextButton);
-                        nextButton.setText("NEXT");
+                        nextButton.setText(R.string.next_onboarding_text);
+                        indicatorSelector(false, true, false);
                         break;
                     case 2:
                         showButton(prevButton);
-                        nextButton.setText("START");
+                        nextButton.setText(R.string.start_onboarding_text);
+                        indicatorSelector(false, false, true);
                         break;
                 }
             }
@@ -86,5 +96,11 @@ public class OnBoardingActivity extends AppCompatActivity {
 
     private void hideButton(AppCompatButton button) {
         button.setVisibility(View.GONE);
+    }
+
+    private void indicatorSelector(boolean first, boolean second, boolean third) {
+        mIndicator1.setSelected(first);
+        mIndicator2.setSelected(second);
+        mIndicator3.setSelected(third);
     }
 }
